59e764cbae514212d363999232b5e83992cdad88
larryschirmer <larryschirmer@gmail.com>
2018-01-11 15:58:52 -0500

    add action logic for handling successful login

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios
+      .post(`${ROOT_URL}/signin`, { email, password })
+      .then(res => {
+        // update state
+        dispatch({ type: AUTH_USER });
+        // save token
+        localStorage.setItem('token', res.data.token);
+        // redirect user to '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {});
+  };
+}
   };
 }
