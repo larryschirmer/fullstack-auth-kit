bea59e1575b95b2bbc93751842081970a07be017
larryschirmer <larryschirmer@gmail.com>
2018-01-11 15:20:06 -0500

    improve usability of root response

--- server/router.js
+++ server/router.js
@@ -6,7 +6,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = app => {
-  app.get('/', requireAuth, (req, res, next) => res.send({ hi: 'there' }));
+  app.get('/', requireAuth, (req, res, next) => res.send({ message: 'ok, great' }));
   app.post('/signin', requireSignin, signin);
   app.post('/signup', signup);
 };
